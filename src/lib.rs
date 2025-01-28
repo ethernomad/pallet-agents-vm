b4f4065d8cd605f17a5cc157efc3aa0678fc8103
Jonathan Brown <jbrown@acuity.network>
2025-01-28 17:46:45 +0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,248 @@
+// Ensure we're `no_std` when compiling for Wasm.
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use alloc::vec;
+use codec::{Decode, Encode};
+use frame_support::dispatch::DispatchResult;
+use frame_system::ensure_signed;
+
+use frame_support::{
+    traits::{Currency, ExistenceRequirement::AllowDeath, Get},
+    PalletId,
+};
+
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
+use scale_info::TypeInfo;
+use sp_runtime::traits::AccountIdConversion;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[cfg(feature = "try-runtime")]
+// use sp_runtime::TryRuntimeError;
+pub mod weights;
+// pub use weights::*;
+
+extern crate alloc;
+
+type BalanceOf<T> =
+    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+#[derive(Encode, Decode, TypeInfo)]
+pub struct Node {
+    name: String,
+    uri: String,
+}
+
+/// Enable `dev_mode` for this pallet.
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::config]
+    pub trait Config: pallet_balances::Config + frame_system::Config {
+        /// The overarching runtime event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        /// Type representing the weight of this pallet
+        // type WeightInfo: WeightInfo;
+
+        /// This is a normal Rust type, nothing specific to FRAME here.
+        type Currency: Currency<Self::AccountId>;
+
+        /// PalletId for pallet. An appropriate value could be ```PalletId(*b"evmbridg")```
+        #[pallet::constant]
+        type PalletId: Get<PalletId>;
+    }
+
+    // Simple declaration of the `Pallet` type. It is placeholder we use to implement traits and
+    // method.
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        pub fn set_deposit_amount(
+            origin: OriginFor<T>,
+            deposit_amount: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            <DepositAmount<T>>::set(deposit_amount);
+            Self::deposit_event(Event::DepositAmountSet { deposit_amount });
+            Ok(())
+        }
+
+        #[pallet::call_index(1)]
+        pub fn set_slash_amount(
+            origin: OriginFor<T>,
+            slash_amount: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            <SlashAmount<T>>::set(slash_amount);
+            Self::deposit_event(Event::SlashAmountSet { slash_amount });
+            Ok(())
+        }
+
+        #[pallet::call_index(2)]
+        pub fn register(origin: OriginFor<T>, name: String, uri: String) -> DispatchResult {
+            let sender = ensure_signed(origin)?;
+            let mut accounts = Accounts::<T>::get();
+            if accounts.contains(&sender) {
+                Err(Error::<T>::NodeAlreadyRegistered)?;
+            }
+
+            let deposit_amount = <DepositAmount<T>>::get();
+            // Move the value from the sender to the pallet.
+            T::Currency::transfer(
+                &sender,
+                &Self::pallet_account_id(),
+                deposit_amount,
+                AllowDeath,
+            )
+            .map_err(|_| DispatchError::Other("Can't transfer value."))?;
+            // Put the new value into storage.
+            <Deposit<T>>::insert(&sender, deposit_amount);
+            let node = Node {
+                name: name.clone(),
+                uri: uri.clone(),
+            };
+            <AccountNode<T>>::insert(&sender, node);
+            accounts.push(sender.clone());
+            Accounts::<T>::set(accounts);
+            // Let's deposit an event to let the outside world know this happened.
+            Self::deposit_event(Event::NodeRegistered {
+                account: sender,
+                name,
+                uri,
+            });
+            Ok(())
+        }
+
+        #[pallet::call_index(3)]
+        pub fn deregister(origin: OriginFor<T>) -> DispatchResult {
+            let sender = ensure_signed(origin)?;
+            let mut accounts = Accounts::<T>::get();
+            let i = accounts
+                .iter()
+                .position(|a| a == &sender)
+                .ok_or(Error::<T>::NodeNotRegistered)?;
+            accounts.swap_remove(i);
+            Accounts::<T>::set(accounts);
+            let deposit = <Deposit<T>>::get(&sender);
+            <Deposit<T>>::remove(&sender);
+            <AccountNode<T>>::remove(&sender);
+            // Move the value from the pallet to the sender.
+            T::Currency::transfer(&Self::pallet_account_id(), &sender, deposit, AllowDeath)
+                .map_err(|_| DispatchError::Other("Can't transfer value."))?;
+            Ok(())
+        }
+
+        #[pallet::call_index(4)]
+        pub fn reward(
+            origin: OriginFor<T>,
+            account: T::AccountId,
+            value: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            let accounts = Accounts::<T>::get();
+            if !accounts.contains(&account) {
+                Err(Error::<T>::NodeNotRegistered)?;
+            }
+            let deposit = <Deposit<T>>::get(&account);
+            <Deposit<T>>::set(&account, deposit + value);
+            Self::deposit_event(Event::NodeRewarded { account, value });
+            Ok(())
+        }
+
+        #[pallet::call_index(5)]
+        pub fn slash(
+            origin: OriginFor<T>,
+            account: T::AccountId,
+            mut value: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+            let mut deposit = <Deposit<T>>::get(&account);
+            let accounts = Accounts::<T>::get();
+            if !accounts.contains(&account) {
+                Err(Error::<T>::NodeNotRegistered)?;
+            }
+            if deposit >= value {
+                deposit -= value;
+            } else {
+                value = deposit;
+                deposit -= deposit;
+            }
+            <Deposit<T>>::set(&account, deposit);
+            Self::deposit_event(Event::NodeSlashed { account, value });
+            Ok(())
+        }
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        DepositAmountSet {
+            deposit_amount: BalanceOf<T>,
+        },
+        SlashAmountSet {
+            slash_amount: BalanceOf<T>,
+        },
+        NodeRegistered {
+            account: T::AccountId,
+            name: String,
+            uri: String,
+        },
+        NodeRewarded {
+            account: T::AccountId,
+            value: BalanceOf<T>,
+        },
+        NodeSlashed {
+            account: T::AccountId,
+            value: BalanceOf<T>,
+        },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        NodeAlreadyRegistered,
+        NodeNotRegistered,
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn deposit_amount)]
+    pub type DepositAmount<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn slash_amount)]
+    pub type SlashAmount<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn deposits)]
+    pub type Deposit<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::AccountId, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn nodes)]
+    pub type AccountNode<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, Node>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn accounts)]
+    pub type Accounts<T: Config> = StorageValue<_, Vec<T::AccountId>, ValueQuery>;
+}
+
+impl<T: Config> Pallet<T> {
+    /// The account ID of the fund pot.
+    ///
+    /// This actually does computation. If you need to keep using it, then make sure you cache the
+    /// value and only call this once.
+    pub fn pallet_account_id() -> T::AccountId {
+        T::PalletId::get().into_account_truncating()
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,221 @@
+use crate::*;
+use frame_support::{assert_err, assert_ok, derive_impl, parameter_types};
+use sp_core::H256;
+use sp_runtime::{
+    traits::{BadOrigin, BlakeTwo256, IdentityLookup},
+    BuildStorage, DispatchError,
+};
+// Reexport crate as its pallet name for construct_runtime.
+use crate as pallet_agents_vm;
+
+type Block = frame_system::mocking::MockBlock<Test>;
+
+// For testing the pallet, we construct a mock runtime.
+frame_support::construct_runtime!(
+    pub enum Test
+    {
+        System: frame_system,
+        Balances: pallet_balances,
+        AgentsVM: pallet_agents_vm,
+    }
+);
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Test {
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type Nonce = u64;
+    type Hash = H256;
+    type RuntimeCall = RuntimeCall;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u64>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+#[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
+impl pallet_balances::Config for Test {
+    type AccountStore = System;
+}
+
+parameter_types! {
+    pub const BridgePalletId: PalletId = PalletId(*b"evmbridg");
+}
+
+impl Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+
+    type PalletId = BridgePalletId;
+    type Currency = Balances;
+}
+
+// This function basically just builds a genesis storage key/value store according to
+// our desired mockup.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let mut t = RuntimeGenesisConfig {
+        // We use default for brevity, but you can configure as desired if needed.
+        system: Default::default(),
+        balances: Default::default(),
+    }
+    .build_storage()
+    .unwrap();
+    let genesis = pallet_balances::GenesisConfig::<Test> {
+        balances: vec![(1, 100), (2, 100)],
+    };
+    genesis.assimilate_storage(&mut t).unwrap();
+    t.into()
+}
+
+#[test]
+fn set_deposit_amount() {
+    new_test_ext().execute_with(|| {
+        let deposit_amount = 133;
+        assert_err!(
+            AgentsVM::set_deposit_amount(RuntimeOrigin::signed(1), deposit_amount),
+            BadOrigin
+        );
+        assert_ok!(AgentsVM::set_deposit_amount(
+            RuntimeOrigin::root(),
+            deposit_amount
+        ));
+        assert_eq!(DepositAmount::<Test>::get(), deposit_amount);
+        let deposit_amount = 134;
+        assert_ok!(AgentsVM::set_deposit_amount(
+            RuntimeOrigin::root(),
+            deposit_amount
+        ));
+        assert_eq!(DepositAmount::<Test>::get(), deposit_amount);
+    });
+}
+
+#[test]
+fn set_slash_amount() {
+    new_test_ext().execute_with(|| {
+        let slash_amount = 133;
+        assert_err!(
+            AgentsVM::set_slash_amount(RuntimeOrigin::signed(1), slash_amount),
+            BadOrigin
+        );
+        assert_ok!(AgentsVM::set_slash_amount(
+            RuntimeOrigin::root(),
+            slash_amount
+        ));
+        assert_eq!(SlashAmount::<Test>::get(), slash_amount);
+        let slash_amount = 134;
+        assert_ok!(AgentsVM::set_slash_amount(
+            RuntimeOrigin::root(),
+            slash_amount
+        ));
+        assert_eq!(SlashAmount::<Test>::get(), slash_amount);
+    });
+}
+
+#[test]
+fn register() {
+    new_test_ext().execute_with(|| {
+        let deposit_amount = 10;
+        assert_ok!(AgentsVM::set_deposit_amount(
+            RuntimeOrigin::root(),
+            deposit_amount
+        ));
+        let name = "Node1";
+        let uri = "https://node.network:443";
+        assert_err!(
+            AgentsVM::register(RuntimeOrigin::signed(3), name.to_string(), uri.to_string()),
+            DispatchError::Other("Can't transfer value.")
+        );
+        assert_ok!(AgentsVM::register(
+            RuntimeOrigin::signed(1),
+            name.to_string(),
+            uri.to_string(),
+        ));
+        assert_err!(
+            AgentsVM::register(RuntimeOrigin::signed(1), name.to_string(), uri.to_string(),),
+            Error::<Test>::NodeAlreadyRegistered
+        );
+    });
+}
+
+#[test]
+fn deregister() {
+    new_test_ext().execute_with(|| {
+        let deposit_amount = 10;
+        assert_ok!(AgentsVM::set_deposit_amount(
+            RuntimeOrigin::root(),
+            deposit_amount
+        ));
+        assert_err!(
+            AgentsVM::deregister(RuntimeOrigin::signed(1),),
+            Error::<Test>::NodeNotRegistered
+        );
+        let name = "Node1";
+        let uri = "https://node.network:443";
+        assert_ok!(AgentsVM::register(
+            RuntimeOrigin::signed(1),
+            name.to_string(),
+            uri.to_string(),
+        ));
+        assert_ok!(AgentsVM::deregister(RuntimeOrigin::signed(1),));
+        let accounts = Accounts::<Test>::get();
+        assert_eq!(accounts.contains(&1), false);
+    });
+}
+
+#[test]
+fn reward() {
+    new_test_ext().execute_with(|| {
+        let reward_amount = 8;
+        assert_err!(
+            AgentsVM::reward(RuntimeOrigin::signed(1), 1, reward_amount),
+            BadOrigin
+        );
+        assert_err!(
+            AgentsVM::reward(RuntimeOrigin::root(), 1, reward_amount),
+            Error::<Test>::NodeNotRegistered
+        );
+        let name = "Node1";
+        let uri = "https://node.network:443";
+        assert_ok!(AgentsVM::register(
+            RuntimeOrigin::signed(1),
+            name.to_string(),
+            uri.to_string(),
+        ));
+        assert_ok!(AgentsVM::reward(RuntimeOrigin::root(), 1, reward_amount));
+    });
+}
+
+#[test]
+fn slash() {
+    new_test_ext().execute_with(|| {
+        let slash_amount = 8;
+        assert_err!(
+            AgentsVM::slash(RuntimeOrigin::signed(1), 1, slash_amount),
+            BadOrigin
+        );
+        assert_err!(
+            AgentsVM::slash(RuntimeOrigin::root(), 1, slash_amount),
+            Error::<Test>::NodeNotRegistered
+        );
+        let name = "Node1";
+        let uri = "https://node.network:443";
+        assert_ok!(AgentsVM::register(
+            RuntimeOrigin::signed(1),
+            name.to_string(),
+            uri.to_string(),
+        ));
+        assert_ok!(AgentsVM::slash(RuntimeOrigin::root(), 1, slash_amount));
+    });
+}
